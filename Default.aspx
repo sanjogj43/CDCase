6bda58c943d1130196ec73023a35cb896c09d603
sanjogj43 <sanjogj43@gmail.com>
2015-03-15 19:51:17 -0700

    First Commit.

---
+++ Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+         <asp:GridView ID="grd_Tracks" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+             <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+             <EditRowStyle BackColor="#999999" />
+             <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+             <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+             <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+             <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+             <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+             <SortedAscendingCellStyle BackColor="#E9E7E2" />
+             <SortedAscendingHeaderStyle BackColor="#506C8C" />
+             <SortedDescendingCellStyle BackColor="#FFFDF8" />
+             <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+         </asp:GridView>
+        <br/>
+        <asp:Button ID="btn_GetData" runat="server" OnClick="btn_getData_Clk" text="Display" style="margin-right:50px"/>
+        <asp:Button ID="btn_Insert" runat="server" OnClick="btn_Insert_Clk" text="Insert"/>
+        <asp:Button ID="btn_Update" runat="server" OnClick="btn_Update_Clk" text="Update"/>
+        <asp:Button ID="btn_Delete" runat="server" OnClick="btn_Delete_Clk" text="Delete"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        CDCase_ClassesDataContext cdcase = new CDCase_ClassesDataContext();
+        cdcase.getdata();
+        var linqQuery = cdcase.getdata();
+
+        grd_Tracks.DataSource = linqQuery;
+        grd_Tracks.DataBind();
+
+    }
+    protected void btn_getData_Clk(object sender, EventArgs e)
+    {
+        CDCase_ClassesDataContext cdcase = new CDCase_ClassesDataContext();
+        grd_Tracks.DataSource = cdcase.Track_tbls;
+        grd_Tracks.DataBind();
+    }
+    protected void btn_Insert_Clk(object sender, EventArgs e)
+    {
+        using(CDCase_ClassesDataContext cd = new CDCase_ClassesDataContext())
+        {
+            Track_tbl trk = new Track_tbl()
+            {
+                cd_id = 2,
+                trk_id = 67,
+                trk_length = 9,
+                trk_num = 10,
+                trk_title = "Melodies"
+            };
+
+            cd.Track_tbls.InsertOnSubmit(trk);
+            cd.SubmitChanges();
+        }
+    }
+
+    protected void btn_Update_Clk(object sender, EventArgs e)
+    {
+        using (CDCase_ClassesDataContext cd_case = new CDCase_ClassesDataContext())
+        {
+            Track_tbl trk = cd_case.Track_tbls.SingleOrDefault(x => x.trk_id == 11);
+            trk.trk_length = 8.9;
+            cd_case.SubmitChanges();
+        }
+    }
+
+    protected void btn_Delete_Clk(object sender, EventArgs e)
+    {
+        using (CDCase_ClassesDataContext cd_case = new CDCase_ClassesDataContext())
+        {
+            Track_tbl trk = cd_case.Track_tbls.SingleOrDefault(x => x.trk_id == 11);
+            cd_case.Track_tbls.DeleteOnSubmit(trk);
+            cd_case.SubmitChanges();
+        }
+    }
+}
